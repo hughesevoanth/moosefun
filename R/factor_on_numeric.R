29881519e12869acb4014c059a429a5fc7b10b53
David Hughes <hughes.evoanth@gmail.com>
2020-07-28 12:12:47 +0100

    adding try() feature to the lm() to catch errors from to few factor observations

--- R/factor_on_numeric.R
+++ R/factor_on_numeric.R
@@ -8,16 +8,25 @@
 #' @examples
 #' factor_on_numeric()
 factor_on_numeric = function( cat_values , num_values ){
-  wdata = data.frame( cat_values = unlist(cat_values) , num_values = rntransform( unlist(num_values) ) )
-  fit = lm(num_values ~ cat_values , data = wdata)
-  adjrsq = summary(fit)$adj.r.squared
-  ####
-  a = anova(fit)
-  etasq = a[1,2]/sum(a[,2])
-  rho = sqrt(etasq)
-  fstat = a[1,4]
-  pval = a[1,5]
-  out = c( etasq , rho,  fstat, pval)
+  ## if values are integers, do not rntransform
+  if(class(num_values) == "numeric"){
+    num_values = rntransform( unlist(num_values) ) 
+  }
+  ###
+  wdata = data.frame( cat_values = unlist(cat_values) , num_values = unlist(num_values) )
+  fit = try( lm(num_values ~ cat_values , data = wdata) )
+  if(length(fit)>1){
+    adjrsq = summary(fit)$adj.r.squared
+    ####
+    a = anova(fit)
+    etasq = a[1,2]/sum(a[,2])
+    rho = sqrt(etasq)
+    fstat = a[1,4]
+    pval = a[1,5]
+    out = c( etasq , rho,  fstat, pval)
+  } else {
+    out = c(0,0,0,1)
+  }
   names(out) = c("Etasq", "rho", "Fstat", "Anova_pval")
   return(out)
 }
